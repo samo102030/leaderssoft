135f7b4150194a2044ee79c1dc94df53a0cb084a
samo102030 <samuel.nashed@yahoo.com>
2015-09-14 03:34:21 +0200

    test reposo

---
+++ JBS2/JBS2/test-repo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JBS2
+{
+    public partial class test_repo : Form
+    {
+        public test_repo()
+        {
+            InitializeComponent();
+        }
+    }
+}
