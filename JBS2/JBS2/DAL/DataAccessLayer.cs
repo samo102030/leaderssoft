f961d3fbebda5cddd97984a333df4f982d4c9ceb
YÙjulia-ibrahim <eng_julia_ibrahim@yahoo.com>
2015-09-16 20:37:15 +0200

    ده المفروض اخر تعديل

---
+++ JBS2/JBS2/DAL/DataAccessLayer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace JBS2.DAL
+{
+    class DataAccessLayer
+    {
+        SqlConnection sqlcon;
+
+        //this Constructor Inisialize the connection object
+        public DataAccessLayer()
+        {
+            sqlcon = new SqlConnection(@"Server=JESUS-PC; Database=john; Integrated Security= True");
+        }
+        // method to open the connection
+        public void open()
+        {
+            if (sqlcon.State != ConnectionState.Open)
+            {
+                sqlcon.Open();
+            }
+        }
+
+        //Method to close the Connection
+        public void close()
+        {
+            if (sqlcon.State == ConnectionState.Open)
+            {
+                sqlcon.Close();
+            }
+        }
+        // Method to read Data from Database
+
+        public DataTable SelectData(string Stored_procedure, SqlParameter[] param)
+        {
+            SqlCommand sqlcmd = new SqlCommand();
+            sqlcmd.CommandType = CommandType.StoredProcedure;
+            sqlcmd.CommandText = Stored_procedure;
+            if (param != null)
+            {
+                for (int i = 0; i < param.Length; i++)
+                {
+                    sqlcmd.Parameters.Add(param[i]);
+                }
+            }
+            SqlDataAdapter da = new SqlDataAdapter(sqlcmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+        // Method To Insert, update, and delete data from Database
+        public void ExecuteCommand(String Stored_procedure, SqlParameter[] param)
+        {
+            SqlCommand sqlcmd = new SqlCommand();
+            sqlcmd.CommandType = CommandType.StoredProcedure;
+            sqlcmd.CommandText = Stored_procedure;
+
+            if (param != null)
+            {
+                // the function AddRange add elmasfofa kolha without need to use the (for loop ) like we do above
+                sqlcmd.Parameters.AddRange(param);
+            }
+            sqlcmd.ExecuteNonQuery();
+        }
+    
+    }
+}
